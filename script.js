45cc4e7e6a70f294977270e55cfa741b9db32c98
TarsisViana <159029529+TarsisViana@users.noreply.github.com>
2024-03-16 15:55:02 +0000

    Add credits to the image

---
+++ script.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('button');
+const pwdCheck = document.querySelector('#confirm-pwd');
+const pwd = document.querySelector('#pwd');
+const pwdLabel = document.querySelector('label[for=pwd]');
+
+pwdCheck.addEventListener('focusout', () => {
+  if(pwdCheck.value != pwd.value){
+    console.log(pwdCheck.value, pwd.value);
+    pwdCheck.classList.toggle('error');
+    pwdLabel.classList.toggle('error');
+    btn.setAttribute('disabled','');
+  }else{
+    btn.removeAttribute('disabled');
+  }
+
+});
+
